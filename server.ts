eb673078ebf35eb791d302d2168a17902bcf0149
Daniel Grynyk <daniel.grynyk@j-labs.pl>
2024-03-25 14:37:43 +0100

    add openai controller

--- server.ts
+++ server.ts
@@ -1,7 +1,6 @@
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
 import { OpenAiController, PlatformController } from './controllers';
-import { Moderation, ModerationCreateResponse } from 'openai/resources';
 
 dotenv.config();
 
@@ -16,18 +15,10 @@ class Server {
     this.openAIController = new OpenAiController();
   }
 
-  private async moderationTask(): Promise<void> {
-    const { token, input } = await this.platformController.getTaskData('moderation');
-    const response: ModerationCreateResponse = await this.openAIController.getModerationForInput(input!);
-    const result: number[] = response.results.map((item: Moderation) => Number(item.flagged));
-    await this.platformController.sendAnswer(token, result);
-  }
-
   private onInit(): void {
     /**
      * Executes method on initialization.
      **/
-    this.moderationTask();
   }
 
   public start: (PORT: number) => Promise<unknown> = (PORT: number) => {
