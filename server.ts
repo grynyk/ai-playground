91129ef427c56b32297f03ce9db96ce8ab538456
Daniel Grynyk <daniel.grynyk@j-labs.pl>
2024-04-15 15:50:41 +0200

    add response type to sendAnswer

--- server.ts
+++ server.ts
@@ -15,37 +15,10 @@ class Server {
     this.openAIController = new OpenAiController();
   }
 
-  private async getGuessedPersonName(systemMessageText: string, userMessageText: string): Promise<string> {
-    const userMessage: HumanMessage = new HumanMessage(userMessageText);
-    const systemMessage: SystemMessage = new SystemMessage(systemMessageText);
-    const messageContent: MessageContent = await this.openAIController.getChatContent([systemMessage, userMessage]);
-    return messageContent as string;
-  }
-
-  private async taskWhoami(): Promise<void> {
-    let userMessageText: string = '';
-    let isKnownPersonName: boolean = false;
-    do {
-      try {
-        const { msg, hint }: PlatformApiData = await this.platformController.getTaskData('whoami');
-        const systemMessageText: string  = `${msg}. Answer strictly with only name of person or null. without any annotations. Answer if you are sure.`;
-        userMessageText += `${hint}`;
-        const personName: string = await this.getGuessedPersonName(systemMessageText, userMessageText);
-        isKnownPersonName = !isNil(personName);
-        if (isKnownPersonName) {
-          await this.platformController.sendAnswer(personName);
-        }
-      } catch(e: unknown) {
-        isKnownPersonName = false;
-      }
-    } while (!isKnownPersonName);
-  }
-
   private onInit(): void {
     /**
      * Executes method on initialization.
      **/
-    this.taskWhoami();
   }
   public start: (PORT: number) => Promise<unknown> = (PORT: number) => {
     return new Promise((resolve, reject): void => {
